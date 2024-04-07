5522e5aa3ea7a34108f851d5370369f999b314af
丹ⓨㄩک廾 <59462658+ayushkumarsingh2422005@users.noreply.github.com>
2024-04-07 16:25:24 +0530

    Ayush update

--- src/Pages/ReasultPage.jsx
+++ src/Pages/ReasultPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Nav from '../Components/Nav'
-
+import './ReasultPage.css'
+import { BiSearch } from 'react-icons/bi'
+import { IoImage } from 'react-icons/io5'
 export default function ReasultPage() {
   return (
     <div>
       <Nav />
+      <div className='top_bar_menu'>
+        <div style={{color: "rgb(254, 53, 86)"}}>
+            <BiSearch/>&nbsp;Search
+        </div>
+        <div>
+            <IoImage/>&nbsp;images
+        </div>
+      </div>
     </div>
   )
 }
