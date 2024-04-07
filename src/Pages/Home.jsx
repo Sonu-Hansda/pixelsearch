817d40bc32e0a3aea9085b29179e17a437cdcea9
丹ⓨㄩک廾 <59462658+ayushkumarsingh2422005@users.noreply.github.com>
2024-04-07 15:45:03 +0530

    Ayush update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import './Home.css'
+import { MdOutlineDarkMode } from "react-icons/md";
 export default function Home() {
   return (
-    <div>
-      this is home page
+    <div className='home_cont'>
+      <div className='top_bar'>
+        <div>Pixel Search</div>
+        <div><MdOutlineDarkMode /></div>
+      </div>
     </div>
   )
 }
