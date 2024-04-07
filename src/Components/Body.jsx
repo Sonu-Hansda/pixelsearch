df8e7e25380566f8775e464b2de936c98c441e0c
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-04-07 16:39:51 +0530

    back

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,8 +1,34 @@
 import React, { useState } from 'react'
 import './Body.css'
+import {searchWeb} from "../utilities/web_search";
 import { CgSearch } from 'react-icons/cg'
 export default function Body() {
     const [inputVal, setInputVal] = useState("");
+
+    const search = async()=>{
+        const api_key="b24e19d5721ad193443c4d48fbf23916bb97975f5afebb93b2c5bfae3aa60c06";
+const base_url="https://serpapi.com/search.json";
+        const url=`${base_url}?engine=google&q=${inputVal}&google_domain=google.com&gl=in&hl=en&api_key=${api_key}`
+        try {
+            const res = await fetch(url);
+            // const data = await res.json();
+            console.log(res);
+            // console.log( {
+            //     status:data['search_metadata']['status'] ?? 'processing',
+            //     images_results:data["images_results"],
+            //     pagination:{
+            //         current:data['serpapi_pagination']['current'] ?? 0,
+            //         next:data['serpapi_pagination']['next'],
+            //         other_pages:data['serpapi_pagination']['other_pages'],
+            //     }
+            // });
+        } catch (error) {
+            console.log( {
+                error:error
+            });
+        }
+    }
+
     return (
         <div className='body'>
             <div className='web_name'><span style={{
@@ -12,7 +38,7 @@ export default function Body() {
             }}>Pixel</span>Search</div>
             <div className='search_cont'>
                 <input type="text" value={inputVal} onChange={(e)=>setInputVal(e.target.value)}/>
-                <button type='submit'><CgSearch/></button>
+                <button type='submit' onClick={search} ><CgSearch/></button>
             </div>
         </div>
     )
--- src/utilities/web_search.js
+++ src/utilities/web_search.js
@@ -3,15 +3,21 @@ const base_url="https://serpapi.com/search.json";
 
 export  const  searchWeb = async(query) => {
     const url=`${base_url}?engine=google&q=${query}&google_domain=google.com&gl=in&hl=en&api_key=${api_key}`
-    const res = await fetch(url);
-    const data = res.json();
-    return {
-        status:data['search_metadata']['status'],
-        images_results:data["images_results"],
-        pagination:{
-            current:data['serpapi_pagination']['current'],
-            next:data['serpapi_pagination']['next'],
-            other_pages:data['serpapi_pagination']['other_pages'],
+    try {
+        const res = await fetch(url,{method:'GET',mode:'no-cors'});
+        const data = await res.json();
+        return {
+            status:data['search_metadata']['status'] ?? 'processing',
+            images_results:data["images_results"],
+            pagination:{
+                current:data['serpapi_pagination']['current'] ?? 0,
+                next:data['serpapi_pagination']['next'],
+                other_pages:data['serpapi_pagination']['other_pages'],
+            }
+        }
+    } catch (error) {
+        return {
+            error:error
         }
     }
 }
